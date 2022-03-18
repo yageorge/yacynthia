bdb66cca74f3045c4a485f01ef45de0a7460630e
George Yaacoub <yageorge@George-MacAirM1.local>
2022-03-18 17:24:03 +0200

    fix brand image

--- src/components/images/brandLogo.tsx
+++ src/components/images/brandLogo.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import brandLogo from '../../../public/assets/images/brand_logo.png'
 
 const BrandLogo = () => {
   return (
     <img
       style={{ width: '8rem', height: '8rem' }}
       alt=''
-      src='/assets/images/brand_logo.png'
+      src={brandLogo}
     />
   )
 }
